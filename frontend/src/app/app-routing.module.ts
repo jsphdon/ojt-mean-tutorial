a910701d2a857f733a84ded84d4207a4678dc4ed
jsphdon <jdcagampang14@gmail.com>
2022-02-22 17:48:35 +0800

    deleteTask not yet implemented

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TaskViewComponent } from './pages/task-view/task-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'lists', pathMatch: 'full' },
+  { path: 'lists', component: TaskViewComponent },
+  { path: `lists/:listId`, component: TaskViewComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
